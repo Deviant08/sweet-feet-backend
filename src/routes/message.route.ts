e1146a161e61056e81298c7d1fa79de1c7fa96e4
His_Image✝ <nnimtheesecond@gmail.com>
2026-09-10 11:45:18 +0100

    Add staff partners route for admin chat

--- src/routes/message.route.ts
+++ src/routes/message.route.ts
@@ -1,11 +1,17 @@
 import { Router } from "express";
 import { catchAsync } from "../middlewares/catchAsyncError.middleware";
 import { protect } from "../middlewares/auth.middleware";
-import { sendMessage, getConversation, markRead } from "../controllers/message.controller";
+import {
+  sendMessage,
+  getConversation,
+  markRead,
+  listStaffPartners,
+} from "../controllers/message.controller";
 
 const messageRouter = Router();
 
 messageRouter.use(protect);
+messageRouter.get("/staff/partners", catchAsync(listStaffPartners));
 messageRouter.post("/", catchAsync(sendMessage));
 messageRouter.get("/", catchAsync(getConversation));
 messageRouter.patch("/read", catchAsync(markRead));
